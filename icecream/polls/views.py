19616d385e05e75ebcaf2170423edfed20a61140
Nicolas Rivera <102812954+NicolasRG0106@users.noreply.github.com>
2023-05-06 09:55:27 -0500

    subida de cambios para creacion de login

--- icecream/polls/views.py
+++ icecream/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
     return HttpResponse("Hello, world. You're at the polls login.")
+
+@login_required
